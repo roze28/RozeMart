1fec90987f3eaf33540cf3177b97a25c37967e18
roze28 <firoze.hossain01@gmail.com>
2022-02-06 16:23:36 +0600

    project initialize

---
+++ RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/RozemartBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.rozemart.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RozemartBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RozemartBackEndApplication.class, args);
+	}
+
+}

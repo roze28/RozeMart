9bb57dd685d318e1ca89d92c485c81ced86c71f6
roze28 <firoze.hossain01@gmail.com>
2022-02-11 11:58:54 +0600

    role entity,role repository and unit testing

--- RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/RozemartBackEndApplication.java
+++ RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/RozemartBackEndApplication.java
@@ -2,8 +2,10 @@ package com.rozemart.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.rozemart.common.entity","com.rozemart.admin.user"})
 public class RozemartBackEndApplication {
 
 	public static void main(String[] args) {

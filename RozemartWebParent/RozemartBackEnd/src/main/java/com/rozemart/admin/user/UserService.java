d93b18d926aa5451d839ec0419536b8c0008b57b
roze28 <firoze.hossain01@yahoo.com>
2022-02-25 18:26:08 +0600

    encoded password added with spring security

--- RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/user/UserService.java
+++ RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/user/UserService.java
@@ -3,6 +3,7 @@ package com.rozemart.admin.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.rozemart.common.entity.Role;
@@ -16,6 +17,9 @@ public class UserService {
 
 	@Autowired
 	private RoleRepository roleRepo;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public List<User> listAll() {
 		return (List<User>) userRepo.findAll();
@@ -26,7 +30,14 @@ public class UserService {
 	}
 
 	public void save(User user) {
+		encodePassword(user);
 		userRepo.save(user);
+
+	}
+	
+	private void encodePassword(User user) {
+		String encodedPassword=passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
 		
 	}
 

35f46fccb13be72cd98c97c3edbdfc2e6a069896
roze28 <firoze.hossain01@yahoo.com>
2022-02-28 20:17:53 +0600

    sql updated

--- rozemartdb.sql
+++ rozemartdb.sql
@@ -40,6 +40,63 @@ LOCK TABLES `roles` WRITE;
 INSERT INTO `roles` VALUES (1,'manage everything','Admin'),(2,'manage product price, customers, shipping, ordersand sales report','Salesperson'),(3,'manage categories, brands, products, articlesand menus','Editor'),(4,'view products, view ordersand update order status','Shipper'),(5,'manage questions and reviews','Assistant');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_roles` (
+  `user_id` int NOT NULL,
+  `role_id` int NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `FKh8ciramu9cc9q3qcqiv4ue8a6` (`role_id`),
+  CONSTRAINT `FKh8ciramu9cc9q3qcqiv4ue8a6` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
+  CONSTRAINT `FKhfh9dx7w3ubf1co1vdev94g3f` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` VALUES (6,1),(7,1),(8,1),(9,1),(5,2),(1,3),(3,3),(5,3),(1,4),(4,4),(5,4),(3,5),(4,5),(5,5);
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(128) NOT NULL,
+  `enabled` bit(1) NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `photos` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_6dotkott2kjsp8vw4d0m25fb7` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'roze2882@gmail.com',_binary '','Md. Firoze','Hossain','firoze28',NULL),(3,'firoze.hossain01@outlook.com',_binary '','Firoze','Ahmed','$2a$10$RPSXb/8FQttMrI9xf/MNPO0ohwRMUg3EyhFEiYzHCxAhC0yzjWAAW',NULL),(4,'firoze.hossain01@yahoo.com',_binary '','Firoze','Hossain','firoze28',NULL),(5,'imtiaze.ahmmed@gmail.com',_binary '\0','Md. Ahmed','Imtiaze Khorarrom','$2a$10$pYXLs.VeLpVR6aKiKZUwxODfLMdac10xYEgsa8gDJEJxJ6QKJb.qu',NULL),(6,'millat@gmail.com',_binary '','Ahsanul Haque','Millat','firoze28',NULL),(7,'rayhan@gmail.com',_binary '','Shykhul Islam','Rayhan','firoze28',NULL),(8,'mydul@gmail.com',_binary '','Mydul','Islam','$2a$10$jQr3TZJWn9pPYDABHvVQ6.IgmFI0J2o.qaLFpfuUfLBxW0J1M4KzO',NULL),(9,'ghh@gmail.com',_binary '','gh','dde','$2a$10$MV.gfPH90skojM8JSY6e7eliqHZh2vvnDLvBDIQ8ETPi2CHlSGvuC',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -50,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-02-11 12:07:06
+-- Dump completed on 2022-02-28 20:17:13

7eee36f872455d71a77aca708db3a381be4a0dc5
roze28 <firoze.hossain01@yahoo.com>
2022-03-06 15:45:31 +0600

    pagination for users list done

--- RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/user/UserController.java
+++ RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/user/UserController.java
@@ -1,9 +1,11 @@
 package com.rozemart.admin.user;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -25,11 +27,32 @@ public class UserController {
 	private UserService service;
 
 	@GetMapping("/users")
-	public String listAll(Model model) {
-		List<User> listUsers = service.listAll();
+	public String listFirstPage(Model model) {
+		return listByPage(1, model);
+
+	}
+
+	@GetMapping("/users/page/{pageNum}")
+	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model) {
+		Page<User> page = service.listByPage(pageNum);
+		List<User> listUsers = page.getContent();
+
+		long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
+		long endCount = startCount + UserService.USERS_PER_PAGE - 1;
+		if (endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
+
+		model.addAttribute("currentPage", pageNum);
+		
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+
+		model.addAttribute("totalItems", page.getTotalElements());
+
 		model.addAttribute("listUsers", listUsers);
 		return "users";
-
 	}
 
 	@GetMapping("/users/new")
@@ -56,8 +79,8 @@ public class UserController {
 			String uploadDir = "user-photos/" + savedUser.getId();
 			FileUploadUtil.cleanDir(uploadDir);
 			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
-		}else {
-			if(user.getPhotos().isEmpty()) {
+		} else {
+			if (user.getPhotos().isEmpty()) {
 				user.setPhotos(null);
 			}
 			service.save(user);

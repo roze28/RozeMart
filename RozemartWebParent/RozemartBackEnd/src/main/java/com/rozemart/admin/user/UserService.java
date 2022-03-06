063add1fa3528ff5c3158374d78497c4dcf485da
roze28 <firoze.hossain01@yahoo.com>
2022-03-06 21:32:22 +0600

    code for filter function(Sorting for users list)

--- RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/user/UserService.java
+++ RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/user/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,7 @@ import com.rozemart.common.entity.User;
 @Service
 @Transactional
 public class UserService {
-	
+
 	public static final int USERS_PER_PAGE = 5;
 
 	@Autowired
@@ -35,8 +36,10 @@ public class UserService {
 		return (List<User>) userRepo.findAll();
 	}
 
-	public Page<User> listByPage(int pageNum) {
-		Pageable pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE);
+	public Page<User> listByPage(int pageNum, String sortField, String sortDir) {
+		Sort sort = Sort.by(sortField);
+		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+		Pageable pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE, sort);
 		return userRepo.findAll(pageable);
 	}
 

06e5210186391724100dc9ef40c1b6a7bcfde3ff
roze28 <firoze.hossain01@yahoo.com>
2022-04-16 17:14:09 +0600

    code export users to excel

--- RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/user/UserCsvExporter.java
+++ RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/user/UserCsvExporter.java
@@ -14,16 +14,11 @@ import org.supercsv.prefs.CsvPreference;
 
 import com.rozemart.common.entity.User;
 
-public class UserCsvExporter {
+public class UserCsvExporter extends AbstractExporter{
 	
 	public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
-		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-SS");
-		String timestamp = dateFormatter.format(new Date());
-		String fileName = "users_" + timestamp + ".csv";
-		response.setContentType("text/csv");
-		String headerKey = "Content-Disposition";
-		String headerValue = "attatchment; filename=" + fileName;
-		response.setHeader(headerKey, headerValue);
+		
+		super.setResponseHeader(response, "text/csv", ".csv");
 		ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
 
 		String[] csvHeader = { "User Id", "Email", "FirstName", "Last Name", "Roles", "Enabled" };

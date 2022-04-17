0ddce44bf7a6b5749f5de8ceed5f436be2d2d97b
roze28 <firoze.hossain01@yahoo.com>
2022-04-17 16:44:56 +0600

    enable remember option

--- RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/security/WebSecurityConfig.java
+++ RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/security/WebSecurityConfig.java
@@ -41,7 +41,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login")
-				.usernameParameter("email").permitAll().and().logout().permitAll();
+				.usernameParameter("email").permitAll().and().logout()
+				.permitAll().and().rememberMe().key("AbcDefgHijklmnOpqrs_1234567890")
+				.tokenValiditySeconds(7*24*60*60);
 	}
 
 	@Override

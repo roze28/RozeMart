d9ad758b3cc028a1db3ac19e6fa581792567c45e
roze28 <firoze.hossain01@yahoo.com>
2022-04-18 14:26:14 +0600

    user authorization done

--- RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/security/WebSecurityConfig.java
+++ RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/security/WebSecurityConfig.java
@@ -40,10 +40,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login")
-				.usernameParameter("email").permitAll().and().logout()
-				.permitAll().and().rememberMe().key("AbcDefgHijklmnOpqrs_1234567890")
-				.tokenValiditySeconds(7*24*60*60);
+		http.authorizeRequests().
+		antMatchers("/users/**","/settings/**").hasAuthority("Admin").
+		anyRequest().authenticated().and().
+		formLogin().loginPage("/login")
+		.usernameParameter("email").permitAll().and().logout()
+		.permitAll().and().rememberMe().key("AbcDefgHijklmnOpqrs_1234567890")
+		.tokenValiditySeconds(7*24*60*60);
 	}
 
 	@Override

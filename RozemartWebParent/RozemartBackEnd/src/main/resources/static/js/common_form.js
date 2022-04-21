ceb99b2b5c2d8e7ae598841fd3546e6bf419be73
firoze <firoze@firoze>
2022-04-22 00:48:37 +0600

    uniqueness of category done

--- RozemartWebParent/RozemartBackEnd/src/main/resources/static/js/common_form.js
+++ RozemartWebParent/RozemartBackEnd/src/main/resources/static/js/common_form.js
@@ -26,3 +26,19 @@ $(document).ready(function () {
 			reader.readAsDataURL(file);
 		}
 		
+		function showModalDialog(title, message) {
+			$("#modalTitle").text(title);
+			$("#modalBody").text(message);
+			$("#modalDialog").modal();
+		}
+		
+		function showErrorModal(message){
+			showModalDialog("Error",message);
+			
+		}
+		
+		function showWarningModal(message){
+			showModalDialog("Warning",message);
+			
+		}
+		

9a0267ef3776962a975aa19c7ec44d604adb69c7
roze28 <firoze.hossain01@yahoo.com>
2022-05-01 12:31:06 +0600

    sort functionality done of categories by name

--- RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/category/CategoryRepository.java
+++ RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/category/CategoryRepository.java
@@ -2,6 +2,7 @@ package com.rozemart.admin.category;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ import com.rozemart.common.entity.Category;
 @Repository
 public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer> {
      @Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
-	public List<Category> findRootCategories();
+	public List<Category> findRootCategories(Sort sort);
      
      public Category findByName(String name);
      

df6480b8a21ce0dec192faa77d14ad9d1c64bf28
roze28 <firoze.hossain01@yahoo.com>
2022-05-18 14:58:49 +0600

    check uniqueness function done for product

--- RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/product/ProductRepository.java
+++ RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/product/ProductRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import com.rozemart.common.entity.Product;
 
 public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+	
+	public Product findByName(String name);
 
 }

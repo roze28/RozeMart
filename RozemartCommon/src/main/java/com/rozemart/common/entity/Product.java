794f702c386ec8992bf155f0624a017309e45508
roze28 <global-firoze.hossain01@yahoo.com>
2022-05-18 21:47:48 +0600

    upload product main image function done

--- RozemartCommon/src/main/java/com/rozemart/common/entity/Product.java
+++ RozemartCommon/src/main/java/com/rozemart/common/entity/Product.java
@@ -14,6 +14,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "products")
@@ -235,9 +236,15 @@ public class Product {
 	public void setImages(Set<ProductImage> images) {
 		this.images = images;
 	}
-	
+
 	public void addExtraImage(String imageName) {
-		this.images.add(new ProductImage(imageName,this));
+		this.images.add(new ProductImage(imageName, this));
+	}
+
+	@Transient
+	public String getMainImagePath() {
+		if(id == null || mainImage == null) return "/images/image-thumbnail.png";
+		return "/product-images/ " + this.id + "/" + this.mainImage;
 	}
 
 }
--- RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/product/ProductController.java
+++ RozemartWebParent/RozemartBackEnd/src/main/java/com/rozemart/admin/product/ProductController.java
@@ -1,13 +1,17 @@
 package com.rozemart.admin.product;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.rozemart.admin.FileUploadUtil;
@@ -43,8 +47,20 @@ public class ProductController {
 	}
 
 	@PostMapping("/products/save")
-	public String saveProduct(Product product, RedirectAttributes ra) {
+	public String saveProduct(Product product,@RequestParam("fileImage") MultipartFile multipartFile, RedirectAttributes ra) throws IOException {
+		if (!multipartFile.isEmpty()) {
+
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			product.setMainImage(fileName);
+			Product savedProduct = productService.save(product);
+			String uploadDir = "../product-images/" + savedProduct.getId();
+
+			FileUploadUtil.cleanDir(uploadDir);
+			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+		} 
+		else {
 		productService.save(product);
+		}
 		ra.addFlashAttribute("message", "The product has been saved successfully");
 		return "redirect:/products";
 	}
